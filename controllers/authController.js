a17ce105bde8eded2880dc4e60f9860ddcdc39bf
w3bdev <w3bdev@protonmail.com>
2022-01-07 23:36:21 +0530

    Sign up connection added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require("../models/Users");
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -6,10 +8,15 @@ module.exports.login_get = (req, res) => {
   res.render("login");
 };
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
-  res.send("New Signup");
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Error, user not created");
+  }
 };
 
 module.exports.login_post = (req, res) => {
--- index.js
+++ index.js
@@ -20,17 +20,15 @@ app.set("view engine", "ejs");
 // connect db
 const dbURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWD}@mongo.jynjm.mongodb.net/${process.env.MONGO_DB}`;
 
-// mongoose
-//   .connect(dbURI)
-//   .then((_) => {
-//     console.log("Connected to DB");
-//     app.listen(PORT);
-//   })
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(dbURI)
+  .then((_) => {
+    console.log("Connected to DB");
+    app.listen(PORT);
+  })
+  .catch((err) => console.log(err));
 
 // Routes
 app.get("/", (req, res) => res.render("home"));
 app.get("/post", (req, res) => res.render("post"));
 app.use(authRoutes);
-
-app.listen(PORT);
